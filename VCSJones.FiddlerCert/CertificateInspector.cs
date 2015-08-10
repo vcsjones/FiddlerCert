31b42776f18d2aee2ab2c7e1b9000116a3533343
Kevin Jones <kevin@vcsjones.com>
2015-08-09 22:26:42 -0400

    Better handling for unsupported operating systems. Windows XP is not supported due to the dependency on DsGetRdnW for parsing distinguished names. This change still allows the extension to load, however it will no longer crash the process and instead just always have an "empty" certificates inspector.

--- VCSJones.FiddlerCert/CertificateInspector.cs
+++ VCSJones.FiddlerCert/CertificateInspector.cs
@@ -1,11 +1,20 @@
-﻿using Fiddler;
+﻿using System;
+using System.Windows.Forms;
+using Fiddler;
 
 namespace VCSJones.FiddlerCert
 {
     public class CertificateInspector : IAutoTamper
     {
+        private bool _isSupportedOperatingSystem = false;
+
         public void OnLoad()
         {
+            _isSupportedOperatingSystem = Environment.OSVersion.Version >= new Version(5, 2);
+            if (!_isSupportedOperatingSystem)
+            {
+                MessageBox.Show("Windows Vista / Server 2003 or greater is required for the Certificate inspector extension to function.");
+            }
         }
 
         public void OnBeforeUnload()
@@ -23,10 +32,14 @@ namespace VCSJones.FiddlerCert
 
         public void AutoTamperResponseBefore(Session oSession)
         {
+            if (!_isSupportedOperatingSystem)
+            {
+                return;
+            }
             var certificate = oSession?.oResponse?.pipeServer?.ServerCertificate;
             if (certificate != null)
             {
-                var base64Certificate = System.Convert.ToBase64String(certificate.Export(System.Security.Cryptography.X509Certificates.X509ContentType.Cert));
+                var base64Certificate = Convert.ToBase64String(certificate.Export(System.Security.Cryptography.X509Certificates.X509ContentType.Cert));
                 oSession[$"{nameof(CertificateInspector)}_Certificate"] = base64Certificate;
             }
         }

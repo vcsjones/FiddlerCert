a96ca6b452349e1196d0aa1469d84518767a22e5
Kevin Jones <kevin@vcsjones.com>
2015-10-22 12:10:10 -0400

    Double-click clipboard support. Double clicking certain UI elements places their contents on the clipboard. This was some functionality that was useful that was lost when migrating to WPF.

---
+++ VCSJones.FiddlerCert/DoubleClickCopyBehavior.cs
@@ -0,0 +1,99 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace VCSJones.FiddlerCert
+{
+    public static class DoubleClickCopyBehavior
+    {
+        public static readonly DependencyProperty IsCopyToClipBoardOnDoubleClickProperty =
+            DependencyProperty.RegisterAttached("IsCopyToClipBoardOnDoubleClick", typeof(bool), typeof(DoubleClickCopyBehavior), new PropertyMetadata { DefaultValue = false, PropertyChangedCallback = OnIsCopyToClipBoardOnDoubleClickChanged });
+
+        public static readonly DependencyProperty LastClickTimeProperty =
+            DependencyProperty.RegisterAttached("LastClickTime", typeof(int?), typeof(DoubleClickCopyBehavior), new PropertyMetadata { DefaultValue = null });
+
+        public static readonly DependencyProperty LastClickLocationProperty =
+            DependencyProperty.RegisterAttached("LastClickLocation", typeof(Point?), typeof(DoubleClickCopyBehavior), new PropertyMetadata { DefaultValue = null });
+
+        public static bool GetIsCopyToClipBoardOnDoubleClick(UIElement element)
+        {
+            return (bool)element.GetValue(IsCopyToClipBoardOnDoubleClickProperty);
+        }
+
+        public static void SetIsCopyToClipBoardOnDoubleClick(UIElement element, bool value)
+        {
+            element.SetValue(IsCopyToClipBoardOnDoubleClickProperty, value);
+        }
+
+        public static void OnIsCopyToClipBoardOnDoubleClickChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            var element = sender as UIElement;
+            if (element == null)
+            {
+                return;
+            }
+            bool enabled = (bool)args.NewValue;
+            if (enabled)
+            {
+                element.MouseLeftButtonUp += UIElement_MouseLeftButtonUp;
+                element.GotFocus += UIElement_GotFocus;
+            }
+            else
+            {
+                element.MouseLeftButtonUp -= UIElement_MouseLeftButtonUp;
+                element.GotFocus -= UIElement_GotFocus;
+            }
+        }
+
+        private static void UIElement_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var element = sender as UIElement;
+            ClearDoubleClickProperties(element);
+        }
+
+        private static void ClearDoubleClickProperties(UIElement element)
+        {
+            element.SetValue(LastClickLocationProperty, null);
+            element.SetValue(LastClickTimeProperty, null);
+        }
+
+        private static void UIElement_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var element = sender as UIElement;
+            var lastClickLocation = element.GetValue(LastClickLocationProperty) as Point?;
+            var lastClickTime = element.GetValue(LastClickTimeProperty) as int?;
+            if (lastClickLocation.HasValue && lastClickTime.HasValue)
+            {
+                if (e.Timestamp - lastClickTime.Value > SystemInformation.DoubleClickTime)
+                {
+                    element.SetValue(LastClickLocationProperty, e.GetPosition(element));
+                    element.SetValue(LastClickTimeProperty, e.Timestamp);
+                    return;
+                }
+                var size = SystemInformation.DoubleClickSize;
+                var rect = new Rect(lastClickLocation.Value, lastClickLocation.Value);
+                rect.Inflate(size.Width / 2, size.Height / 2);
+                if (rect.Contains(e.GetPosition(element)))
+                {
+                    var contentControl = element as ContentControl;
+                    if (contentControl != null)
+                    {
+                        Debug.WriteLine($"Clipboard set to: \"{contentControl.Content.ToString()}\"");
+                        System.Windows.Clipboard.SetText(contentControl.Content.ToString());
+                    }
+                    var textBlock = element as TextBlock;
+                    if (textBlock != null)
+                    {
+                        Debug.WriteLine($"Clipboard set to: \"{textBlock.Text}\"");
+                        System.Windows.Clipboard.SetText(textBlock.Text);
+                    }
+                    ClearDoubleClickProperties(element);
+                    return;
+                }
+            }
+            element.SetValue(LastClickLocationProperty, e.GetPosition(element));
+            element.SetValue(LastClickTimeProperty, e.Timestamp);
+        }
+    }
+}

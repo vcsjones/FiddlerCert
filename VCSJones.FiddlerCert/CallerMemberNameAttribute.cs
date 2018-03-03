f01223b32d49db233d8b213a92dd18ad80d5086b
Kevin Jones <kevin@vcsjones.com>
2018-03-02 20:42:21 -0500

    Update to work against the latest Fiddler.

--- VCSJones.FiddlerCert/CallerMemberNameAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿ // ReSharper disable once CheckNamespace
-namespace System.Runtime.CompilerServices
-{
-    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-    public class CallerMemberNameAttribute : Attribute
-    {
-        
-    }
-}
